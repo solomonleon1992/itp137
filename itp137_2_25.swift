69a3b96ce17a95e306da8103a0b6704756483521
Leon Solomon <>
2023-02-25 15:37:46 +0000

    added code

---
+++ itp137_2_25.swift
@@ -0,0 +1,10 @@
+import Foundation 
+import Glibc
+
+//var temp = 0
+//temp = readLine()
+//print(temp)
+var numOne = Int(readLine()!)!
+var numTwo = Int(readLine()!)!
+var sum = numOne + numTwo
+print("The sum is " + String(sum))
---
+++ main.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Glibc
+
+var temp = 0.0
+temp = Double(readLine()!)!
+print(temp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var courseCode = true
+// print(type(of: courseCode))
+// //print(courseCode)
