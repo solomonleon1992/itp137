4c944ede92d2bd18faac2bf587c08cc704bf969e
Leon Solomon <126338123+solomonleon1992@users.noreply.github.com>
2023-02-26 01:04:46 +0000

    updated code

--- main.swift
+++ main.swift
@@ -1,12 +1,10 @@
-var num = 0
-num = Int(readLine()!)!
-// if num % 2 == 0 {
-//   print(String(num) + " is Even.")
-// } else {
-//   print(String(num) + " is Odd.")
-// }
+import Foundation
+import Glibc
 
-while num > 0 {
-  print(num)
-  num /= 2
-}
+//var courseCode = true
+//print(type(of:courseCode))
+//print(courseCode)
+
+var temp = 0
+temp = Int (readLine()!)!
+print(temp)
