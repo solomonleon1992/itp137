7a412d917303e56be4851559fa83b6573fdba570
Leon Solomon <126338123+solomonleon1992@users.noreply.github.com>
2023-04-01 13:41:45 +0000

    wrote new code

--- main.swift
+++ main.swift
@@ -1,64 +1,11 @@
-print("Please enter a string") 
+var wholeNumber = 13
 
-
-  if let input = readLine() {
-
-     if let charValue = mostFrequentCharacter(input){
-  
-      
-      print("The most frequent character is " +  String(charValue))
-
-    
-    }
-
-  }
-else {
-
-  print ("error try again")
+// Write your code below 
+switch wholeNumber {
+  case let x where x % 2 == 0:
+    print("Composite")
+  case let x where x % 3 == 0:
+    print("Composite")
+  default:
+    print("Prime")
 }
-
-
-
-
-
-
-
-
-
-func mostFrequentCharacter(_ input: String) -> Character? { 
-
-
-  var maxCount = 0
-  
-  var maxChar: Character?
-
-  
-
-  for char in input {
-
-    print("This is the char \(char)")
-    var count = 0 
-
-    for otherChar in input {
-        print("This is the otherChar \(otherChar)")
-        print("This is the count before line 49 \(count)")
-        if otherChar == char {
-
-      
-        count+=1
-        print("This is the count before line 51 \(count)")
-      }  
-    }
-    if count > maxCount {
-
-      maxChar = char
-      maxCount = count
-    }
-
-  
-  
-  }
-
-return maxChar
-  
-} 
